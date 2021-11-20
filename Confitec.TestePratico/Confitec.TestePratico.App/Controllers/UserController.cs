f73a4863757198209445b4b91ddb6279b77f6f58
Vitor Hugo <vitorifsudeste@gmail.com>
2021-11-20 14:02:51 -0300

    Ajuste nas dependencias do projeto

--- Confitec.TestePratico/Confitec.TestePratico.App/Controllers/UserController.cs
+++ Confitec.TestePratico/Confitec.TestePratico.App/Controllers/UserController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Confitec.TestePratico.Domain.Entities;
 using Confitec.TestePratico.Service.Dto;
 using Confitec.TestePratico.Service.Interfaces;
 
